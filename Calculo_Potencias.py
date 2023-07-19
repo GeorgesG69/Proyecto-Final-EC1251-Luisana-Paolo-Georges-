eb067eee422a09cdc95e8a3ccb709966f2b95dfb
GeorgesG69 <cddde123@gmail.com>
2023-07-19 09:04:18 -0400

    #

--- Calculo_Potencias.py
+++ Calculo_Potencias.py
@@ -3,6 +3,7 @@ import math
 import cmath
 
                                     # -Potencias de las fuentes de voltaje- #
+                                    
 def V_fuentes(Imp_V_fuente, Voltaje_Pico, Desfase, Vth, Indice_V_fuente):
 
     # Transformación del voltaje pico de las fuentes a forma rectangular.
